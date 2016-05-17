b654e9314d1d11018d4e9b559b10689c32bad66d
Unknown6656 <Unknown6656@users.noreply.github.com>
2016-05-17 11:12:40 +0200

    this is a temporary file!
    temporary LINQ expressions for basic type/method matching

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿#define TEST
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System;
+
+namespace MethodInvoker
+{
+
+    public static class Program
+    {
+
+        public static void Main(string[] args)
+        {
+#if TEST
+            args = new string[] { Assembly.GetExecutingAssembly().Location, "Program", "test", "(string)", "\"test\"", "null" };
+#endif
+            Assembly asm = Assembly.LoadFrom(args[0]);
+            IEnumerable<Type> tps = asm.GetTypes();
+            IEnumerable<Type> classes = from t in tps
+                                        where t.FullName.EndsWith(args[1])
+                                        select t;
+            IEnumerable<MethodInfo> methods = (from t in classes
+                                               let m = t.GetMethods()
+                                               select from _ in m
+                                                      where _.Name == args[2]
+                                                      select _).SelectMany(_ => _);
+
+            if (methods.Count() > 1)
+            {
+
+
+            }
+            else
+                methods.First().Invoke(args.Contains("new") ? Activator.CreateInstance(methods.FirstOrDefault().DeclaringType) : null, /* parse args */);
+
+        }
+
+        public static int test(string s)
+        {
+            return s == null ? -1 : s.Length;
+        }
+
+        public static string test(int i)
+        {
+            return i < 0 ? null : new string('k', i);
+        }
+    }
+}
